ea864422de1d2dbacccefdc6627e6943e5e208e8
zolfaqar1223 <kontaktzolfi@gmail.com>
2025-08-02 18:26:25 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+import BabyForm from "../components/BabyForm";
 import WelcomeCard from "../components/WelcomeCard";
 
 export default function Home() {
+  const [babyName, setBabyName] = useState("");
+
   return (
     <div style={{ padding: "2rem" }}>
       <h1>BabyGuiden+ Webapp</h1>
-      <WelcomeCard name="Noah" />
+      {babyName ? (
+        <WelcomeCard name={babyName} />
+      ) : (
+        <BabyForm onSubmit={setBabyName} />
+      )}
     </div>
   );
 }
